1883292b89d899faf10bfc77bb1bf99d6f107080
Kanika Rawat <114948582+kanikaarawat@users.noreply.github.com>
2024-09-26 04:40:44 +0530

    Delete Arrays/Beginner  directory

--- Arrays/Beginner /Create.py
+++
@@ -1,6 +0,0 @@
-#Create an array and traverse
-
-from array import *
-my_array = array('i', [1,2,3,4,5])
-for i in my_array:
-    print(i)
--- Arrays/Beginner /accessIndex.py
+++
@@ -1,6 +0,0 @@
-#Accessing Individual elements through indexes
-
-from array import *
-my_array = array('i', [1,2,3,4,5])
-for i in range(len(my_array)):
-    print(my_array[i])
